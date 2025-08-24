fc22fa3b2d0e6689ce5eb64608d2a751c0d6236d
serdkan <serdar.kandiran@outlook.com>
2025-08-25 00:59:35 +0300

    dashboard

--- routes/dashboard/dashboard-module-controller.js
+++ routes/dashboard/dashboard-module-controller.js
@@ -3,26 +3,31 @@ const Dashboard = require('./model/dashboard.model');
 
 async function dashboardModuleController(req, res) {
   try {
-    const { date, url } = req.query;
+    const { startDate, endDate, url } = req.query;
+    const start = startDate ? startDate.split('T')[0] : null;
+    const end = endDate ? endDate.split('T')[0] : null;
     if (url === 'daily-order-report') {
       const result = await Dashboard.getDashboardData(
         {
-          firmId: {
-            type: sql.Int,
-            value: 1,
-          },
-          Date: {
+          startDate: {
             type: sql.date,
-            value: date,
+            value: start,
+          },
+          endDate: {
+            type: sql.date,
+            value: end,
           },
         },
         url,
       );
+
       return res.send(result.data);
     }
     return res.send(400);
   } catch (err) {
-    return res.serverError(err);
+    return res
+      .status(500)
+      .json({ error: err.message || 'Internal Server Error' });
   }
 }
 
--- routes/dashboard/dashboard.sql.js
+++ routes/dashboard/dashboard.sql.js
@@ -2,11 +2,10 @@ const dailyOrderReport = `SELECT
 SIPARISTIPI AS orderType,
 SUM(MIKTAR) AS quantity,
 SUM(M2) AS m2,
-SUM(TUTAR) AS amount,
-FORMAT(TARIH, 'yyyy-MM-dd') AS date
-FROM SR_SIPARISRAPOR_GUNLUK
-WHERE TARIH=@Date
-GROUP BY SIPARISTIPI,TARIH
+SUM(TUTAR) AS amount
+FROM SR_SIPARISRAPOR_TARIH
+WHERE @startDate<=TARIH AND @endDate>=TARIH
+GROUP BY SIPARISTIPI
 `;
 
 module.exports = { dailyOrderReport };

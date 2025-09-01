d9146e264fe12ef08abae564437ac534872c4c8b
serdkan <serdar.kandiran@outlook.com>
2025-09-01 17:47:45 +0300

    price

--- routes/item/item.sql.js
+++ routes/item/item.sql.js
@@ -1,3 +1,3 @@
-const getItemListSql = `SELECT  ROW_NUMBER() OVER (ORDER BY STOK_KODU) AS id,STOK_KODU AS itemCode,dbo.TRK(STOK_ADI) AS itemName,OLCU_BR1 AS unitName FROM TBLSTSABIT WHERE GRUP_KODU=@group`;
+const getItemListSql = `SELECT  ROW_NUMBER() OVER (ORDER BY STOK_KODU) AS id,STOK_KODU AS itemCode,dbo.TRK(STOK_ADI) AS itemName,OLCU_BR1 AS unitName,SATIS_FIAT1 as price1 FROM TBLSTSABIT WHERE GRUP_KODU=@group`;
 
 module.exports = { getItemListSql };
